905a0a899177659008b6b6d0cb43fb8158f8b609
jbeja <bejaranojesus99@gmail.com>
2016-06-15 19:16:22 -0430

    cleaning

--- js/highlight-worker.js
+++
@@ -1,7 +0,0 @@
-self.onmessage = function(event) {
-  importScripts(event.data.hugo.Site.BaseURL + 'js/highlight.pack.js');
-  var result = [].map.call(event.data.codeblocks, function (code) {
-      return self.hljs.highlightAuto(code);
-  });
-  postMessage(result);
-}
